d4803d8e80fe7062186fe4a492f6e5cb2e7d19fc
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-11-19 16:15:14 +0530

    Rename app variable to application in app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def home():
     return "Hello from NEW Python App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    application.run(host="0.0.0.0", port=5000)
