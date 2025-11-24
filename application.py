3b71b183427e596e8aa4e8535643e3bd88d6c3b4
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-11-24 11:44:49 +0530

    Added line

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "Hello from NEW Python App!\nCI/CD pipeline working, TEST 3"
+    return "Hello from NEW Python App!\nCI/CD pipeline working, TEST 3, TEST 4"
